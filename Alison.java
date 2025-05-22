c90eeb79154bd2d9c0b16ecadd681e816d71460c
alisonnguerraag <andrea.guerra0104@gmail.com>
2025-05-21 18:42:32 -0600

    Create Alison.java
    Ejercicio de tu baile favorito

---
+++ Alison.java
@@ -0,0 +1,17 @@
+public class Alison {
+    public static void main(String[] args) {
+        String baile = "jazz";
+
+        if (baile.equals("ballet")) {
+            System.out.println("Te gusta el ballet, ¡qué elegancia y control!");
+        }
+
+        if (baile.equals("contemporaneo")) {
+            System.out.println("Te gusta el contemporáneo, ¡pura expresión y emoción!");
+        }
+
+        if (baile.equals("jazz")) {
+            System.out.println("Te gusta el jazz, ¡energía y estilo en cada paso!");
+        }
+    }
+}
